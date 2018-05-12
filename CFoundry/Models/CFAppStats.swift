c4f9e216b61d4803ed15af135948745fccbee413
Dwayne Forde <dwayne.forde@gmail.com>
2018-05-12 16:50:29 -0400

    CF App stats response parsing

--- CFoundry/Models/CFAppStats.swift
+++ CFoundry/Models/CFAppStats.swift
@@ -1,9 +1,20 @@
-//
-//  CFAppStats.swift
-//  CFoundry
-//
-//  Created by Dwayne Forde on 2018-04-24.
-//  Copyright © 2018 Dwayne Forde. All rights reserved.
-//
-
 import Foundation
+import ObjectMapper
+
+public class CFAppStats: ImmutableMappable {
+    public var instances:[CFAppInstance]
+
+    public required init(map: Map) throws {
+        instances = []
+    }
+    
+    public func mapping(map: Map) {
+        let keysArray = Array(map.JSON.keys)
+        for key in keysArray {
+            let json = map.JSON[key] as! [String : Any]
+            let instance = try! Mapper<CFAppInstance>().map(JSON: json)
+            
+            instances.append(instance)
+        }
+    }
+}
--- CFoundryTests/Models/CFAppInstanceTests.swift
+++ CFoundryTests/Models/CFAppInstanceTests.swift
@@ -4,51 +4,55 @@ import XCTest
 @testable import CFoundry
 
 class CFAppInstanceTests: CFModelTestBase {
-    var instance: CFAppInstance?
+    var runningInstance: CFAppInstance?
+    var stoppedInstance: CFAppInstance?
     
     override func setUp() {
         super.setUp()
-        
-        instance = localResponseObject(t: CFAppInstance.self, name: "app_stats", keyPath: "0")
+    
+        let appStats = localResponseObject(t: CFAppStats.self, name: "app_stats", keyPath: "")
+        runningInstance = appStats.instances[0]
+        stoppedInstance = appStats.instances[1]
     }
     
     func testURIs() {
-        XCTAssertEqual(instance!.uris!, ["app_name.example.com", "other_name.example.com"])
+        XCTAssertEqual(runningInstance!.uris!, ["app_name.example.com", "other_name.example.com"])
     }
     
     func testState() {
-        XCTAssertEqual(instance!.state, "RUNNING")
+        XCTAssertEqual(runningInstance!.state, "RUNNING")
+        XCTAssertEqual(stoppedInstance!.state, "STOPPED")
     }
     
     func testReadableState() {
-        XCTAssertEqual(instance!.translatedState(), "started")
+        XCTAssertEqual(runningInstance!.translatedState(), "started")
     }
     
     func testMemoryUsageMB() {
-        XCTAssertEqual(instance!.memoryUsageMB(), 28.49609375)
+        XCTAssertEqual(runningInstance!.memoryUsageMB(), 28.49609375)
     }
     
     func testMemoryQuotaMB() {
-        XCTAssertEqual(instance!.memoryQuotaMB(), 512.0)
+        XCTAssertEqual(runningInstance!.memoryQuotaMB(), 512.0)
     }
     
     func testDiskUsageMB() {
-        XCTAssertEqual(instance!.diskUsageMB(), 63.31640625)
+        XCTAssertEqual(runningInstance!.diskUsageMB(), 63.31640625)
     }
     
     func testDiskQuotaMB() {
-        XCTAssertEqual(instance!.diskQuotaMB(), 1024.0)
+        XCTAssertEqual(runningInstance!.diskQuotaMB(), 1024.0)
     }
     
     func testMemoryUsagePercentage() {
-        XCTAssertEqual(instance!.memoryUsagePercentage(), 5.5999999999999996)
+        XCTAssertEqual(runningInstance!.memoryUsagePercentage(), 5.5999999999999996)
     }
     
     func testDiskUsagePercentage() {
-        XCTAssertEqual(instance!.diskUsagePercentage(), 6.2000000000000002)
+        XCTAssertEqual(runningInstance!.diskUsagePercentage(), 6.2000000000000002)
     }
     
     func testCPUUsagePercentage() {
-        XCTAssertEqual(instance!.cpuUsagePercentage(), 14.0)
+        XCTAssertEqual(runningInstance!.cpuUsagePercentage(), 14.0)
     }
 }
